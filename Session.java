543ff72fb53f356dac929106398afba1b0c2b27d
monouk-prog <p20240062@gmail.com>
2025-12-30 23:10:37 +0700

    Changed Session.java

--- Session.java
+++ Session.java
@@ -1,38 +1,13 @@
 /**
  * Class Name: Session
  * Description: Represents a study session during the day.
- * Author: Seng Setha
+ * Author: Soun Puthimonouk
  * Version: 1.0
- * Date: 25-12-2025
+ * Date: 30-12-2025
  */
 public class Session {
-
-    // ===== Instance variables =====
-    private int startTime;
-    private int endTime;
-
-    // ===== Constructor =====
-    public Session(int startTime, int endTime) {
-        setDuration(startTime, endTime);
-    }
-
-    // ===== Getters =====
-    public int getStartTime() {
-        return startTime;
-    }
-
-    public int getEndTime() {
-        return endTime;
-    }
-
-    // ===== Setter with Exception (Task 8) =====
-    public void setDuration(int startTime, int endTime) {
-        if (startTime >= endTime) {
-            throw new IllegalArgumentException(
-                "Start time must be less than end time."
-            );
-        }
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
+    String session_topic;
+    String date;
+    String description;
+    int duration;
 }
